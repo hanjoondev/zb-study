715202f4e3d30a0b0bb9a3f7c68fe57debd31c7a
hanjoondev <hanjoondev@gmail.com>
2022-04-03 00:59:05 +0900

    Initial Commit solution

--- part01/P1812.java
+++ part01/P1812.java
@@ -1,19 +1,21 @@
+import java.util.Scanner;
+
 public class P1812 {
     public static void solution() {
-
+        Scanner userInput = new Scanner(System.in);
+        System.out.printf("Enter any string: ");
+        String s = userInput.nextLine();
+        System.out.println(s.chars().map(c -> Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c))
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString());
+        userInput.close();
     }
 
     public static void reference() {
-        int a = (int)'a';
-        System.out.println("a = " + a);
-        int z = (int)'z';
-        System.out.println("z = " + z);
-        int A = (int)'A';
-        System.out.println("A = " + A);
-        int Z = (int)'Z';
-        System.out.println("Z = " + Z);
-        int etc = (int)'%';
-        System.out.println("etc = " + etc);
+        System.out.println("a = " + (int) 'a');
+        System.out.println("z = " + (int) 'z');
+        System.out.println("A = " + (int) 'A');
+        System.out.println("Z = " + (int) 'Z');
+        System.out.println("% = " + (int) '%');
     }
 
     public static void main(String[] args) {
