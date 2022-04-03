4ed808d2a9143bb68c6fda2a4a7ffec98547197c
hanjoondev <hanjoondev@gmail.com>
2022-04-03 15:49:41 +0900

    Initial Commit solution 18_2_2

--- part01/P1822.java
+++ part01/P1822.java
@@ -1,9 +1,18 @@
 public class P1822 {
 
     public static String solution(int num){
-        String result = "";
-
-        return result;
+        String result = "", d = "IVXLCDM";
+        for (int i = 1; i < 6; i += 2) {
+            int q = num % 10 / 5;
+            int r = num % 10 % 5;
+            num /= 10;
+            if (r < 4)
+                result += String.valueOf(d.charAt(i - 1)).repeat(r) + String.valueOf(d.charAt(i)).repeat(q);
+            else
+                result += String.valueOf(d.charAt(i + q)) + String.valueOf(d.charAt(i - 1));
+        }
+        result += String.valueOf(d.charAt(d.length() - 1)).repeat(num);
+        return new StringBuilder(result).reverse().toString();
     }
 
     public static void main(String[] args) {
