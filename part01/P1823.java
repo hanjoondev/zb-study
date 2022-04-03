a171142ad34280da2faece213edb7a2a7d9eac3e
hanjoondev <hanjoondev@gmail.com>
2022-04-03 21:10:01 +0900

    Initial Commit solution 18_2_3

--- part01/P1823.java
+++ part01/P1823.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class P1823 {
     public static String solution(String input, String cmd) {
-        return null;
+        ArrayList<String> c = new ArrayList<>(Arrays.asList(cmd.split(" ")));
+        int pos = input.length();
+        while (c.size() > 0) {
+            String first = c.get(0);
+            int iLen = input.length();
+            if (first.equals("L") && pos != 0) {
+                pos--;
+            } else if (first.equals("D") && pos != iLen) {
+                pos++;
+            } else if (first.equals("B") && pos != 0 && iLen > 0) {
+                input = input.substring(0, pos - 1) + input.substring(pos, iLen);
+            } else if (first.equals("P")) {
+                input = input.substring(0, pos) + c.get(1) + input.substring(pos, iLen);
+                pos++;
+                c.remove(1);
+            }
+            c.remove(0);
+        }
+        return input;
     }
 
     public static void main(String[] args) {
