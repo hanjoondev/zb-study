919b8e3be633d0eb0c8ea5e5fa18689b9c4efe52
hanjoondev <hanjoondev@gmail.com>
2022-04-05 00:35:55 +0900

    Initial Commit solution 18_3_1

--- part01/P1831.java
+++ part01/P1831.java
@@ -1,6 +1,13 @@
 public class P1831 {
     public static void solution(int[] nums) {
-
+        int last = nums[0], biggest = nums[nums.length - 1];
+        System.out.printf("%d" + (last < biggest ? ", " : ""), last);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != last) {
+                System.out.printf("%d" + (nums[i] < biggest ? ", " : "\n"), nums[i]);
+                last = nums[i];
+            }
+        }
     }
 
     public static void main(String[] args) {
