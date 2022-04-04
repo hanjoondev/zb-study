3a9f63211dccba6656738fe6313a995b13abf2f7
hanjoondev <hanjoondev@gmail.com>
2022-04-04 23:47:56 +0900

    Initial Commit solution 18_2_5

--- part01/P1825.java
+++ part01/P1825.java
@@ -1,6 +1,17 @@
 public class P1825 {
     public static int solution(int[] ratings) {
-        return 0;
+        int len = ratings.length;
+        int[] fToR = new int[len], rToF = new int[len];
+        fToR[0] = 1;
+        rToF[len - 1] = 1;
+        for (int i = 1; i < len; i++) {
+            fToR[i] = ratings[i] > ratings[i - 1] ? fToR[i - 1] + 1 : 1;
+            rToF[len - i - 1] = ratings[len - i - 1] > ratings[len - i] ? rToF[len - i] + 1 : 1;
+        }
+        int ans = 0;
+        for (int i = 0; i < len; i++)
+            ans += Math.max(fToR[i], rToF[i]);
+        return ans;
     }
 
     public static void main(String[] args) {
