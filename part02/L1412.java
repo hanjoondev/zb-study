277e7f73152dd0c96ccaa1a42a7d814708236a47
hanjoondev <hanjoondev@gmail.com>
2022-04-05 11:31:37 +0100

    Initial Commit solution L14_1_2

--- part02/L1412.java
+++ part02/L1412.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Practice2 {
-
+public class L1412 {
     public static void solution(int[][] matrix) {
-
+        List<Integer> row = new ArrayList<>(), col = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[0].length; j++)
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[0].length; j++)
+                if (row.contains(i) || col.contains(j))
+                    matrix[i][j] = 0;
+        System.out.println(IntStream.range(0, matrix.length)
+            .mapToObj(i -> String.format("%s", Arrays.toString(matrix[i])))
+            .collect(Collectors.joining("\n")));
     }
 
     public static void main(String[] args) {
