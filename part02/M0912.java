de25b16799d50861dd565f83f137d4fb0168d3c0
hanjoondev <hanjoondev@gmail.com>
2022-04-05 15:48:01 +0100

    Initial Commit solution M09_1_2

--- part02/M0912.java
+++ part02/M0912.java
@@ -1,8 +1,27 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class M0912 {
     public static void solution(int[] arr) {
-
+        int len = arr.length;
+        int bigger = len - 1, smaller = len - 1;
+        Boolean found = false;
+        while (smaller > 0 && !found) {
+            for (int i = smaller - 1; i >= 0; i--) {
+                if (arr[i] > arr[smaller]) {
+                    bigger = i;
+                    found = true;
+                    break;
+                }
+            }
+            smaller -= found ? 0 : 1;
+        }
+        if (found) {
+            int temp = arr[smaller];
+            arr[smaller] = arr[bigger];
+            arr[bigger] = temp;
+        }
+        System.out.println(Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining(", ")));
     }
 
     public static void main(String[] args) {
