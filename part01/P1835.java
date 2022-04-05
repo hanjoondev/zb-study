09a369203c78d49c6f0288b57225b98736f3ac3d
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-05 21:28:59 +0900

    Initial Commit solution 18_3_5

--- part01/P1835.java
+++ part01/P1835.java
@@ -1,8 +1,24 @@
 public class P1835 {
-    public static int solution(int[] height) {
-        return 0;
+    public static int solution(int[] h) {
+        int area = 0, lmax = 0, rmax = 0, l = 0, r = h.length - 1;
+        while (l < r) {
+            if (h[l] < h[r]) {
+                if (h[l] > lmax)
+                    lmax = h[l];
+                else
+                    area += lmax - h[l];
+                l++;
+            } else {
+                if (h[r] > rmax)
+                    rmax = h[r];
+                else
+                    area += rmax - h[r];
+                r--;
+            }
+        }
+        return area;
     }
-    
+
     public static void main(String[] args) {
         // Test code
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
