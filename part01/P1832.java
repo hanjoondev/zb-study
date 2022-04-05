044287c4b740758c25bb3902b255ad261fdd431c
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-05 15:35:27 +0900

    Initial Commit solution 18_3_2

--- part01/P1832.java
+++ part01/P1832.java
@@ -2,8 +2,16 @@ import java.util.ArrayList;
 
 public class P1832 {
     public static ArrayList<Integer> solution(int[] nums) {
-
-        return null;
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    nums[i] *= -1;
+                    arr.add(nums[j]);
+                }
+            }
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
