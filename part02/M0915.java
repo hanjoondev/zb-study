408f9f3f99677e53e9996a65da207f031f88ad5c
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-06 14:46:21 +0900

    Initial Commit solution M09_1_5

--- part02/M0915.java
+++ part02/M0915.java
@@ -1,18 +1,43 @@
 public class M0915 {
     public static int solution(int[][] grid) {
-
-        return 0;
+        int ans = 0, h = grid.length, w = grid[0].length;
+        for (int r = 0; r < h; r++) {
+            for (int c = 0; c < w; c++) {
+                if (grid[r][c] == 1) {
+                    int tmp = 4;
+                    int[][] nxt = {{r, c + 1}, {r + 1, c}, {r, c - 1}, {r - 1, c}};
+                    for (int[] n : nxt)
+                        if (n[0] >= 0 && n[0] < h && n[1] >= 0 && n[1] < w && grid[n[0]][n[1]] == 1)
+                            tmp--;
+                    ans += tmp;
+                }
+            }
+        }
+        return ans;
     }
     
     // 재귀 풀이
     public static int solution2(int[][] grid) {
-
+        int h = grid.length, w = grid[0].length;
+        for (int r = 0; r < h; r++)
+            for (int c = 0; c < w; c++)
+                if (grid[r][c] == 1)
+                    return recursion(grid, 
+                            new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}, 
+                            r, c, h, w);
         return 0;
     }
 
-    public static int recursion(int[][] grid, int[][] directions, int i, int j) {
-
-        return 0;
+    public static int recursion(int[][] g, int[][] d, int r, int c, int h, int w) {
+        if (r < 0 || c < 0 || r >= h || c >= w || g[r][c] == 0)
+            return 1;
+        if (g[r][c] < 0)
+            return 0;
+        int ans = 0;
+        g[r][c] = -1;
+        for (int[] dir : d)
+            ans += recursion(g, d, r + dir[0], c + dir[1], h, w);
+        return ans;
     }
 
     public static void main(String[] args) {
