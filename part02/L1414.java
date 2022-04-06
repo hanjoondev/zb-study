672df65c6373212dfca1598e37db169f6bf1f67c
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-07 04:34:24 +0900

    Initial Commit solution L14_1_4

--- part02/L1414.java
+++ part02/L1414.java
@@ -1,6 +1,19 @@
 public class L1414 {
     public static void solution(String str) {
-
+        String openers = "({[", closers = ")}]", expecting = "";
+        for (char c : str.toCharArray()) {
+            int oi = openers.indexOf(c), ci = closers.indexOf(c);
+            if (oi != -1)
+                expecting = closers.charAt(oi) + expecting;
+            else if (ci != -1 && expecting.length() > 0) {
+                if (c != expecting.charAt(0)) {
+                    System.out.println("Fail");
+                    return;
+                } else
+                    expecting = expecting.substring(1);
+            } 
+        }
+        System.out.println("Pass");
     }
 
     public static void main(String[] args) {
