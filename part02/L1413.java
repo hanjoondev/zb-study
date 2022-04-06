4b7c699b343c2a3c02ffd914a365feb0151f997b
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-07 04:34:12 +0900

    Initial Commit solution L14_1_3

--- part02/L1413.java
+++ part02/L1413.java
@@ -1,6 +1,23 @@
 public class L1413 {
     public static void solution(int[] data) {
-
+        int len = data.length, remaining = data.length - 1, nxt = data[0];
+        int cur = 0, dir = nxt > 0 ? 1 : -1;
+        int[] popped = new int[len];
+        popped[cur] = 1;
+        System.out.printf("%d ", cur + 1);
+        while (remaining > 0) {
+            int moveCount = 0;
+            while (moveCount < Math.abs(nxt)) {
+                cur += dir;
+                cur = cur == len ? 0 : cur < 0 ? len - 1 : cur;
+                moveCount += popped[cur] == 0 ? 1 : 0;
+            }
+            popped[cur] = 1;
+            System.out.printf("%d ", cur + 1);
+            nxt = data[cur];
+            dir = nxt > 0 ? 1 : -1;
+            remaining--;
+        }
     }
 
     public static void main(String[] args) {
