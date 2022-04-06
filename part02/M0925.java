028778e7f201ddca9bf22a53ca09bcc71d930580
hanjoondev <hanjoondev@gmail.com>
2022-04-06 18:35:43 +0100

    Initial Commit solution M09_2_5

--- part02/M0925.java
+++ part02/M0925.java
@@ -1,7 +1,18 @@
 public class M0925 {
 
     public static void solution(int n) {
+        recurTowers(n, 1, 3, 2);
+    }
 
+    public static void recurTowers(int n, int fr, int to, int aux) {
+        if (n == 1) {
+            System.out.printf("%d %d\n", fr, to);
+            return;
+        } else {
+            recurTowers(n - 1, fr, aux, to);
+            recurTowers(1, fr, to, aux);
+            recurTowers(n - 1, aux, to, fr);
+        }
     }
 
     public static void main(String[] args) {
