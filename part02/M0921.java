9f737cde53d829516e0d9137dd50e986b4ff77bb
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-06 17:40:29 +0900

    Initial Commit solution M09_2_1

--- part02/M0921.java
+++ part02/M0921.java
@@ -1,9 +1,15 @@
+import java.math.BigInteger;
+
 public class M0921 {
 
     public static int solution(int n) {
-        int result = 0;
-
-        return result;
+        BigInteger fact2n = new BigInteger("1"), factSquared = new BigInteger("1");
+        for (int i = 2 * n; i > 1; i--)
+            fact2n = fact2n.multiply(BigInteger.valueOf(i));
+        for (int i = n; i > 1; i--)
+            factSquared = factSquared.multiply(BigInteger.valueOf(i));
+        factSquared = factSquared.multiply(factSquared);
+        return fact2n.divide(factSquared).divide(BigInteger.valueOf(n + 1)).intValue();
     }
 
 
