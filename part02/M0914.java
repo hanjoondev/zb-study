ef0165eebe6f62455469728c0fe3ea760b5db04f
hanjoondev <hanjoondev@gmail.com>
2022-04-06 05:58:21 +0100

    Initial Commit solution M09_1_4

--- part02/M0914.java
+++ part02/M0914.java
@@ -1,9 +1,20 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class M0914 {
+    private static HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
     public static boolean solution(int n) {
-
-        return false;
+        if (n < 11)
+            return n == 1 || n == 7 || n == 10 ? true : false;
+        if (memo.get(n) == null) {
+            int rem = 0, sum = 0, num = n;
+            while (num > 0) {
+                rem = num % 10;
+                sum += rem * rem;
+                num /= 10;
+            }
+            memo.put(n, sum);
+        }
+        return solution(memo.get(n));
     }
 
     public static void main(String[] args) {
@@ -11,5 +22,7 @@ public class M0914 {
         System.out.println(solution(19));
         System.out.println(solution(2));
         System.out.println(solution(61));
+        System.out.println(solution(97));
+        System.out.println(solution(818));
     }
 }
