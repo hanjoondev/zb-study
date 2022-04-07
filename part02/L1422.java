d47a6eff94a79ac9b52fb8b85fed34e0abd18298
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-07 17:09:52 +0900

    Initial Commit solution L14_2_2

--- part02/L1422.java
+++ part02/L1422.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class L1422 {
     public static void solution(int[] nums) {
+        Stack<Integer> stack = new Stack<Integer>();
+        ArrayList<String> ans = new ArrayList<String>();
 
+        int count = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int target = nums[i];
+            while (count <= target) {
+                stack.push(count);
+                ans.add("+");
+                count++;
+            }
+            if (stack.peek() == target) {
+                stack.pop();
+                ans.add("-");
+            } else {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.printf(String.join(" ", ans));
     }
 
     public static void main(String[] args) {
