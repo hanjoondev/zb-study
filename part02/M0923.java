4134ba9f20cd2995961b11177d134aca6517760a
hanjoondev <hanjoondev@gmail.com>
2022-04-09 04:20:20 +0100

    Add submission detail and fixed minor bug

--- part02/M0923.java
+++ part02/M0923.java
@@ -4,13 +4,13 @@ import java.util.List;
 public class M0923 {
     public static String solution(String equation) {
         int eqIdx = equation.indexOf("=");
-        if (eqIdx < 0)
-            return "Infinite solutions";
         int[] l = evaluate(equation.substring(0, eqIdx));
         int[] r = evaluate(equation.substring(eqIdx + 1, equation.length()));
         int[] result = {l[0] - r[0], (l[1] - r[1]) * -1};
         if (result[0] == 0 && result[1] == 0)
             return "Infinite solutions";
+        if (result[0] == 0)
+            return "No solution";
         double ans = (double) result[1] / result[0];
         return "x=" + (ans % 1 == 0 ? String.valueOf((int) ans) : ans);
     }
@@ -58,6 +58,53 @@ public class M0923 {
         return new int[]{coeff, constant};
     }
 
+/* leetcode submission
+    // Source: 640. Solve the Equation https://leetcode.com/problems/solve-the-equation/
+    // Submission detail: https://leetcode.com/submissions/detail/676699694/
+    //     Runtime: 1 ms, faster than 94.52% of Java online submissions for Solve the Equation.
+    //     Memory Usage: 40.4 MB, less than 91.78% of Java online submissions for Solve the Equation.
+    public String solveEquation(String equation) {
+        int eqIdx = equation.indexOf("=");
+        int[] l = evaluate(equation.substring(0, eqIdx));
+        int[] r = evaluate(equation.substring(eqIdx + 1, equation.length()));
+        int[] result = {l[0] - r[0], (l[1] - r[1]) * -1};
+        if (result[0] == 0 && result[1] == 0)
+            return "Infinite solutions";
+        if (result[0] == 0)
+            return "No solution";
+        double ans = (double) result[1] / result[0];
+        return "x=" + (ans % 1 == 0 ? String.valueOf((int) ans) : ans);
+    }
+
+    public static int[] evaluate(String str) {
+        List<String> splits = new ArrayList<>();
+        String t = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '+' || c == '-') {
+                if (t.length() > 0)
+                    splits.add(t);
+                t = String.valueOf(c);
+            } else {
+                t += String.valueOf(c);
+            }
+        }
+        splits.add(t);
+        int coeff = 0, constant = 0;
+        for (String s : splits) {
+            if (s.indexOf("x") < 0) {
+                constant += Integer.parseInt(s);
+            } else {
+                String tmp = s.substring(0, s.indexOf("x"));
+                tmp = tmp.equals("") || tmp.equals("+") ? "1" : 
+                      tmp.equals("-") ? "-1" : tmp;
+                coeff += Integer.parseInt(tmp);
+            }
+        }
+        return new int[]{coeff, constant};
+    }
+*/
+    
     public static void main(String[] args) {
         // Test code
         String equation = "x+5-3+x=6+x-2";
