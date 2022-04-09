fa81ff1e56614cbb9a795d7af8c8fcc3ec0ecc9e
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-09 09:06:59 +0900

    Add submission detail

--- part02/L1424.java
+++ part02/L1424.java
@@ -26,3 +26,33 @@ public class L1424 {
         System.out.println(solution(participant, completion));
     }
 }
+
+/* programmers submission
+    // Source: 완주하지 못한 선수 https://programmers.co.kr/learn/courses/30/lessons/42576
+    // Submission detail:
+    //     정확성 테스트
+    //     테스트 1 〉 통과 (0.19ms, 78.8MB)
+    //     테스트 2 〉 통과 (0.21ms, 75.7MB)
+    //     테스트 3 〉 통과 (1.74ms, 77.8MB)
+    //     테스트 4 〉 통과 (2.56ms, 84.6MB)
+    //     테스트 5 〉 통과 (2.53ms, 74.2MB)
+    //     효율성 테스트
+    //     테스트 1 〉 통과 (151.84ms, 81.4MB)
+    //     테스트 2 〉 통과 (290.11ms, 89.3MB)
+    //     테스트 3 〉 통과 (274.72ms, 93.8MB)
+    //     테스트 4 〉 통과 (314.78ms, 95.3MB)
+    //     테스트 5 〉 통과 (245.42ms, 95.7MB)
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] p, String[] c) {
+        Arrays.sort(p);
+        Arrays.sort(c);
+        int i = 0, len = c.length;
+        for (; i < len; i++)
+            if (!p[i].equals(c[i]))
+                return p[i];
+        return p[i];
+    }
+}
+*/
--- part02/M0911.java
+++ part02/M0911.java
@@ -16,6 +16,28 @@ public class M0911 {
         return ans;
     }
 
+/* leetcode submission
+    // Source: 118. Pascal's Triangle https://leetcode.com/problems/pascals-triangle/
+    // Submission detail: https://leetcode.com/submissions/detail/676638672/
+    //     Runtime: 1 ms, faster than 64.28% of Java online submissions for Pascal's Triangle.
+    //     Memory Usage: 41.8 MB, less than 61.44% of Java online submissions for Pascal's Triangle.
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i)
+                    row.add(1);
+                else
+                    row.add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+            }
+            ans.add(row);
+        }
+        return ans;
+    }
+}
+*/
+
     public static void main(String[] args) {
         // Test code
         System.out.println(solution(1));
