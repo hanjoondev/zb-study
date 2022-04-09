8e7d6858e99919992ee3b1f421ed9e1269040fb1
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-09 09:07:40 +0900

    Fix a bug and add submission detail

--- part02/M0912.java
+++ part02/M0912.java
@@ -3,26 +3,54 @@ import java.util.stream.Collectors;
 
 public class M0912 {
     public static void solution(int[] arr) {
-        int len = arr.length;
-        int bigger = len - 1, smaller = len - 1;
-        Boolean found = false;
-        while (smaller > 0 && !found) {
-            for (int i = smaller - 1; i >= 0; i--) {
-                if (arr[i] > arr[smaller]) {
-                    bigger = i;
-                    found = true;
-                    break;
-                }
-            }
-            smaller -= found ? 0 : 1;
+        int bigger = arr.length - 2;
+        if (bigger < 0 || bigger == 0 && arr[0] == arr[1]) {
+            System.out.println(Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining(", ")));
+            return;
         }
-        if (found) {
-            int temp = arr[smaller];
-            arr[smaller] = arr[bigger];
-            arr[bigger] = temp;
+        int smaller = bigger + 1;
+        for (; bigger >= 0; bigger--)
+            if (arr[bigger] > arr[bigger + 1])
+                break;
+        if (bigger < 0) {
+            System.out.println(Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining(", ")));
+            return;
         }
+        while (smaller >= 0 && arr[smaller] >= arr[bigger])
+            smaller--;
+        while (smaller >= 0 && arr[smaller - 1] == arr[smaller])
+            smaller--;
+        int temp = arr[smaller];
+        arr[smaller] = arr[bigger];
+        arr[bigger] = temp;
         System.out.println(Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining(", ")));
     }
+
+/* leetcode submission
+    // Source: 1053. Previous Permutation With One Swap https://leetcode.com/problems/previous-permutation-with-one-swap/
+    // Submission detail: https://leetcode.com/submissions/detail/676648044/
+    //     Runtime: 0 ms, faster than 100.00% of Java online submissions for Previous Permutation With One Swap.
+    //     Memory Usage: 43.9 MB, less than 82.95% of Java online submissions for Previous Permutation With One Swap.
+    public int[] prevPermOpt1(int[] arr) {
+        int bigger = arr.length - 2;
+        if (bigger < 0 || bigger == 0 && arr[0] == arr[1])
+            return arr;
+        int smaller = bigger + 1;
+        for (; bigger >= 0; bigger--)
+            if (arr[bigger] > arr[bigger + 1])
+                break;
+        if (bigger < 0)
+            return arr;
+        while (smaller >= 0 && arr[smaller] >= arr[bigger])
+            smaller--;
+        while (smaller >= 0 && arr[smaller - 1] == arr[smaller])
+            smaller--;
+        int temp = arr[smaller];
+        arr[smaller] = arr[bigger];
+        arr[bigger] = temp;
+        return arr;
+    }
+*/
 
     public static void main(String[] args) {
         // Test code
