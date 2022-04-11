71b6717dae022ed5b8066f5a37f0cf7e7a015410
hanjoondev <hanjoondev@gmail.com>
2022-04-12 00:05:35 +0100

    Update adopted in-place sort

--- part01/P1832.java
+++ part01/P1832.java
@@ -1,29 +1,45 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class P1832 {
-    public static ArrayList<Integer> solution(int[] nums) {
-        Arrays.sort(nums);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        for (int i = 1; i < nums.length; i++) 
-            if (nums[i] == nums[i - 1])
-                arr.add(nums[i]);
-        return arr;
+    public static ArrayList<Integer> solution(int[] n) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        int i = 0, len = n.length;
+        while (i < len) {
+            if (n[i] - 1 == i || n[n[i] - 1] == n[i]) {
+                i++;
+            } else {
+                int t = n[i];
+                n[i] = n[n[i] - 1];
+                n[t - 1] = t;
+            }
+        }
+        for (int j = 0; j < len; j++)
+            if (n[j] != j + 1)
+                ans.add(n[j]);
+        return ans;
     }
 
 /* leetcode submission
     // Source: 442. Find All Duplicates in an Array https://leetcode.com/problems/find-all-duplicates-in-an-array/
-    // Submission detail: https://leetcode.com/submissions/detail/676525781/
-    //     Runtime: 26 ms, faster than 20.62% of Java online submissions for Find All Duplicates in an Array.
-    //     Memory Usage: 67.9 MB, less than 28.25% of Java online submissions for Find All Duplicates in an Array.
-    public List<Integer> findDuplicates(int[] nums) {
-        Arrays.sort(nums);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        for (int i = 1; i < nums.length; i++) 
-            if (nums[i] == nums[i - 1])
-                arr.add(nums[i]);
-        return arr;
-    }
+    // Submission detail: https://leetcode.com/submissions/detail/678603228/
+    //     Runtime: 8 ms, faster than 68.48% of Java online submissions for Find All Duplicates in an Array.
+    //     Memory Usage: 66.2 MB, less than 60.56% of Java online submissions for Find All Duplicates in an Array.
+    public List<Integer> findDuplicates(int[] n) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        int i = 0;
+        while (i < n.length) {
+            if (n[i] - 1 == i || n[n[i] - 1] == n[i]) {
+                i++;
+            } else {
+                int t = n[i];
+                n[i] = n[n[i] - 1];
+                n[t - 1] = t;
+            }
+        }
+        for (int j = 0; j < n.length; j++)
+            if (n[j] != j + 1)
+                ans.add(n[j]);
+        return ans;
     }
 */
 
