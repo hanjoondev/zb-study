c7dd034e5320ed2777682cbec1b49bfa91438f35
hanjoondev <hanjoondev@gmail.com>
2022-04-11 23:24:55 +0100

    Fix wrong submission detail

--- part01/P1825.java
+++ part01/P1825.java
@@ -19,15 +19,19 @@ public class P1825 {
     // Submission detail: https://leetcode.com/submissions/detail/676509118/
     //     Runtime: 4 ms, faster than 68.10% of Java online submissions for Candy.
     //     Memory Usage: 52 MB, less than 42.08% of Java online submissions for Candy.
-    public int reverse(int x) {
-        Boolean neg = x < 0;
-        x = neg ? -x : x;
-        String ans = new StringBuilder().append(x).reverse().toString();
-        try {
-            return Integer.parseInt(ans) * (neg ? -1 : 1);
-        } catch (NumberFormatException e) {
-            return 0;
+    public int candy(int[] ratings) {
+        int len = ratings.length;
+        int[] fToR = new int[len], rToF = new int[len];
+        fToR[0] = 1;
+        rToF[len - 1] = 1;
+        for (int i = 1; i < len; i++) {
+            fToR[i] = ratings[i] > ratings[i - 1] ? fToR[i - 1] + 1 : 1;
+            rToF[len - i - 1] = ratings[len - i - 1] > ratings[len - i] ? rToF[len - i] + 1 : 1;
         }
+        int ans = 0;
+        for (int i = 0; i < len; i++)
+            ans += Math.max(fToR[i], rToF[i]);
+        return ans;
     }
 */
 
