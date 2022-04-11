2aca09580cd2af01cc4748b97847e8567f200c42
hanjoondev <hanjoondev@gmail.com>
2022-04-11 23:25:33 +0100

    Update optimized submissions

--- part01/P1821.java
+++ part01/P1821.java
@@ -1,49 +1,38 @@
 import java.util.HashMap;
-import java.util.Map;
 
 public class P1821 {
     public static void solution(String s) {
-        Map<String, Integer> d = new HashMap<String, Integer>() {{
-            put("I", 1); put("IV", 4); put("V", 5); put("IX", 9);
-            put("X", 10); put("XL", 40); put("L", 50); put("XC", 90);
-            put("C", 100); put("CD", 400); put("D", 500); put("CM", 900);
-            put("M", 1000);
+        HashMap<String, Integer> toInt = new HashMap<>() {{
+            put("I", 1); put("V", 5); put("X", 10); put("L", 50);
+            put("C", 100); put("D", 500); put("M", 1000);
         }};
-        String[] doubleCharacters = {"IV", "IX", "XL", "XC", "CD", "CM"};
-        String tmp = s;
-        int ans = 0;
-        for (String dc : doubleCharacters) {
-            if (tmp.contains(dc))
-                ans += d.get(dc);
-            tmp = tmp.replace(dc, "");
+        int ans = toInt.get(s.substring(s.length() - 1, s.length()));
+        int prev = ans, cur = ans;
+        for (int i = s.length() - 2; i >= 0 ; i--) {
+            cur = toInt.get(s.substring(i, i + 1));
+            ans += cur * (cur < prev ? -1 : 1);
+            prev = cur;
         }
-        for (int i = 0; i < tmp.length(); i++)
-            ans += d.get(String.valueOf(tmp.charAt(i)));
         System.out.println(ans);
     }
 
 /* leetcode submission
     // Source: 13. Roman to Integer https://leetcode.com/problems/roman-to-integer/
-    // Submission detail: https://leetcode.com/submissions/detail/676441993/
-    //     Runtime: 22 ms, faster than 8.93% of Java online submissions for Roman to Integer.
-    //     Memory Usage: 50.2 MB, less than 8.74% of Java online submissions for Roman to Integer.
+    // Submission detail: https://leetcode.com/submissions/detail/678578888/
+    //     Runtime: 4 ms, faster than 93.65% of Java online submissions for Roman to Integer.
+    //     Memory Usage: 42.7 MB, less than 87.18% of Java online submissions for Roman to Integer.
     public int romanToInt(String s) {
-        Map<String, Integer> d = new HashMap<String, Integer>() {{
-            put("I", 1); put("IV", 4); put("V", 5); put("IX", 9);
-            put("X", 10); put("XL", 40); put("L", 50); put("XC", 90);
-            put("C", 100); put("CD", 400); put("D", 500); put("CM", 900);
-            put("M", 1000);
+        HashMap<String, Integer> toInt = new HashMap<>() {{
+            put("I", 1); put("V", 5); put("X", 10); put("L", 50);
+            put("C", 100); put("D", 500); put("M", 1000);
         }};
-        String[] doubleCharacters = {"IV", "IX", "XL", "XC", "CD", "CM"};
-        String tmp = s;
-        int ans = 0;
-        for (String dc : doubleCharacters) {
-            if (tmp.contains(dc))
-                ans += d.get(dc);
-            tmp = tmp.replace(dc, "");
+        int ans = toInt.get(s.substring(s.length() - 1, s.length()));
+        int prev = ans, cur = ans;
+        for (int i = s.length() - 2; i >= 0 ; i--) {
+            cur = toInt.get(s.substring(i, i + 1));
+            ans += cur * (cur < prev ? -1 : 1);
+            prev = cur;
         }
-        for (int i = 0; i < tmp.length(); i++)
-            ans += d.get(String.valueOf(tmp.charAt(i)));
         return ans;
     }
 */
--- part01/P1822.java
+++ part01/P1822.java
@@ -1,41 +1,36 @@
 public class P1822 {
-    public static String solution(int num){
-        String result = "", d = "IVXLCDM";
-        for (int i = 1; i < 6; i += 2) {
-            int q = num % 10 / 5;
-            int r = num % 10 % 5;
-            num /= 10;
-            if (r < 4)
-                result += String.valueOf(d.charAt(i - 1)).repeat(r) + String.valueOf(d.charAt(i)).repeat(q);
-            else
-                result += String.valueOf(d.charAt(i + q)) + String.valueOf(d.charAt(i - 1));
+    public static String solution(int n){
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String ans="";
+        int i = 0;
+        while (n > 0 && i < 13) {
+            while (n >= value[i]) {
+                ans += roman[i];
+                n -= value[i];
+            }
+            i++;
         }
-        result += String.valueOf(d.charAt(d.length() - 1)).repeat(num);
-        return new StringBuilder(result).reverse().toString();
+        return ans;
     }
 
 /* leetcode submission
     // Source: 12. Integer to Roman https://leetcode.com/problems/integer-to-roman/
-    // Submission detail: https://leetcode.com/submissions/detail/676445203/
-    //     Runtime: 24 ms, faster than 13.34% of Java online submissions for Integer to Roman.
-    //     Memory Usage: 47.6 MB, less than 16.81% of Java online submissions for Integer to Roman.
-    public int romanToInt(String s) {
-        Map<String, Integer> d = new HashMap<String, Integer>() {{
-            put("I", 1); put("IV", 4); put("V", 5); put("IX", 9);
-            put("X", 10); put("XL", 40); put("L", 50); put("XC", 90);
-            put("C", 100); put("CD", 400); put("D", 500); put("CM", 900);
-            put("M", 1000);
-        }};
-        String[] doubleCharacters = {"IV", "IX", "XL", "XC", "CD", "CM"};
-        String tmp = s;
-        int ans = 0;
-        for (String dc : doubleCharacters) {
-            if (tmp.contains(dc))
-                ans += d.get(dc);
-            tmp = tmp.replace(dc, "");
+    // Submission detail: https://leetcode.com/submissions/detail/678587046/
+    //     Runtime: 4 ms, faster than 93.10% of Java online submissions for Integer to Roman.
+    //     Memory Usage: 42.2 MB, less than 87.75% of Java online submissions for Integer to Roman.
+    public String intToRoman(int n) {
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String ans="";
+        int i = 0;
+        while (n > 0 && i < 13) {
+            while (n >= value[i]) {
+                ans += roman[i];
+                n -= value[i];
+            }
+            i++;
         }
-        for (int i = 0; i < tmp.length(); i++)
-            ans += d.get(String.valueOf(tmp.charAt(i)));
         return ans;
     }
 */
