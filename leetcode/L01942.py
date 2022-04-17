4734622890cb4b8dafd161711583a2c51fa3c61e
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-17 11:59:26 +0900

    Update 2. 3. 13_2. 1 solution (heapq)

--- leetcode/L01942.py
+++ leetcode/L01942.py
@@ -1,4 +1,5 @@
 from time import perf_counter_ns as ns
+from heapq import heappush as hpush, heappop as hpop
 
 
 class Solution:
@@ -7,21 +8,17 @@ class Solution:
         chairs, empty = [], []
         for arrival, leaving in sorted([time for time in times]):
             while chairs and arrival >= chairs[0][0]:
-                time_left, idx = chairs.pop(0)
-                empty.append(idx)
-            if empty:
-                empty.sort()
+                hpush(empty, hpop(chairs)[1])
             if arrival == target:
                 return empty[0] if empty else len(chairs)
-            chair_idx = len(chairs) if not empty else empty.pop(0)
-            chairs.append((leaving, chair_idx))
-            chairs.sort()
+            hpush(chairs, (leaving, len(chairs) if not empty else hpop(empty)))
 
 
 if __name__ == '__main__':
     ITERATIONS = 1_000
     print(f'Running the basic tests {ITERATIONS:,} times...')
     tests = (
+        ([[4,5],[12,13],[5,6],[1,2],[8,9],[9,10],[6,7],[3,4],[7,8],[13,14],[15,16],[14,15],[10,11],[11,12],[2,3],[16,17]], 15, 0),
         ([[1, 4], [2, 3], [4, 6]], 1, 1),
         ([[3, 10], [1, 5], [2, 6]], 0, 2)
     )
