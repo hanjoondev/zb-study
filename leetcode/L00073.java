e54022eef9e9b15003b88b91b75e22fe1c94978b
hanjoondev <hanjoondev@gmail.com>
2022-04-17 16:41:14 +0100

    Update 2. 2. 14_1. 2 solution

--- leetcode/L00073.java
+++ leetcode/L00073.java
@@ -1,19 +1,23 @@
 package leetcode;
 
-import java.util.*;
-
 public class L00073 {
     public void setZeroes(int[][] matrix) {
-        List<Integer> row = new ArrayList<>(), col = new ArrayList<>();
-        for (int i = 0; i < matrix.length; i++)
-            for (int j = 0; j < matrix[0].length; j++)
-                if (matrix[i][j] == 0) {
-                    row.add(i);
-                    col.add(j);
+        Boolean firstRow = false, firstCol = false;
+        for (int r = 0; r < matrix.length; r++)
+            for (int c = 0; c < matrix[0].length; c++)
+                if (matrix[r][c] == 0) {
+                    if (r == 0) firstRow = true;
+                    if (c == 0) firstCol = true;
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
                 }
-        for (int i = 0; i < matrix.length; i++)
-            for (int j = 0; j < matrix[0].length; j++)
-                if (row.contains(i) || col.contains(j))
-                    matrix[i][j] = 0;
+        for (int r = 1; r < matrix.length; r++)            
+            for (int c = 1; c < matrix[0].length; c++)
+                if (matrix[r][0] == 0 || matrix[0][c] == 0)
+                    matrix[r][c] = 0;
+        if (firstRow) matrix[0] = new int[matrix[0].length];
+        if (firstCol)
+            for (int r = 0; r < matrix.length; r++)
+                matrix[r][0] = 0;
     }
 }
