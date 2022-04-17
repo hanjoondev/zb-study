44b36bbe939e42b29dc62245eab40fe57b2be2ca
hanjoondev <hanjoondev@gmail.com>
2022-04-17 15:41:10 +0100

    Update 2. 2. 14_1. 4 solution (Stack)

--- leetcode/L00020.java
+++ leetcode/L00020.java
@@ -1,20 +1,33 @@
 package leetcode;
 
+import java.util.*;
+
 public class L00020 {
     public boolean isValid(String s) {
-        String openers = "({[", closers = ")}]", expecting = "";
+        HashMap<Character, Character> pairs = new HashMap<>() {{
+            put(')', '(');
+            put('}', '{');
+            put(']', '[' );
+        }};
+        List<Character> openers = new ArrayList<>() {{
+            add('(');
+            add('{');
+            add('[');
+        }};
+        Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            int oi = openers.indexOf(c), ci = closers.indexOf(c);
-            if (oi != -1)
-                expecting = closers.charAt(oi) + expecting;
-            else if (ci != -1 && expecting.length() > 0)
-                if (c != expecting.charAt(0))
+            if (openers.contains(c)) {
+                stack.push(c);
+            } else {
+                if (stack.contains(pairs.get(c))) {
+                    if (pairs.get(c) != stack.peek())
+                        return false;
+                    stack.pop();
+                } else {
                     return false;
-                else
-                    expecting = expecting.substring(1);
-            else
-                return false;
+                }
+            }
         }
-        return expecting.length() > 0 ? false : true;
+        return stack.isEmpty();
     }
 }
