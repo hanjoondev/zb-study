00c66bf4d1ebe6c3bbfd82fdc60dda8f927b261c
Hanjoon Kim <hanjoondev@gmail.com>
2022-04-17 11:59:41 +0900

    Initial Commit 2. 3. 13_2. 1 solution

--- leetcode/L01942.java
+++ leetcode/L01942.java
@@ -1,7 +1,20 @@
 package leetcode;
 
+import java.util.*;
+
 public class L01942 {
     public int smallestChair(int[][] times, int targetFriend) {
-        return 0;
+        int targetArrival = times[targetFriend][0], heapSize = times.length;
+        PriorityQueue<int[]> chairs = new PriorityQueue<>( (x, y) -> x[0] - y[0] );
+        PriorityQueue<Integer> empty = new PriorityQueue<>();
+        Arrays.sort(times, (x, y) -> x[0] - y[0]);
+        for (int i = 0; i < heapSize; i++) {
+            while (!chairs.isEmpty() && times[i][0] >= chairs.peek()[0])
+                empty.offer(chairs.poll()[1]);
+            if (times[i][0] == targetArrival)
+                return empty.isEmpty() ? chairs.size() : empty.poll();
+            chairs.offer(new int[] { times[i][1], empty.isEmpty() ? chairs.size() : empty.poll() });
+        }
+        return -1;
     }
 }
