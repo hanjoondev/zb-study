3d07d2c358cd6f297a094f29a4a9772c86652d94
hanjoondev <hanjoondev@gmail.com>
2022-04-18 01:46:57 +0100

    Update 2. 1. 09_2. 4 solution

--- leetcode/L01922.java
+++ leetcode/L01922.java
@@ -1,13 +1,18 @@
 package leetcode;
 
-import java.math.*;
-
 public class L01922 {
     public int countGoodNumbers(long n) {
-        BigInteger mod = new BigInteger("1000000007");
-        BigInteger b5 = new BigInteger("5"), b4 = new BigInteger("4");
-        BigInteger ans = b5.modPow(new BigInteger(String.valueOf(n % 2 == 0 ? n / 2 : (n + 1) / 2)), mod)
-               .multiply(b4.modPow(new BigInteger(String.valueOf(n % 2 == 0 ? n / 2 : (n - 1) / 2)), mod));
-        return ans.mod(mod).intValue();
+        int mod = 1_000_000_007;
+        long ans = n % 2 == 0 ? 1 : 5;
+        long x = 20;
+        for (long i = n / 2; i > 0; i /= 2) {
+            if (i % 2 > 0) {
+                ans *= x;
+                ans %= mod;
+            }
+            x *= x;
+            x %= mod;
+        }
+        return (int) ans;
     }
 }
