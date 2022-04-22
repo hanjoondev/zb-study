42ff96c6d4f4f1b3a2df4f82a0ffaf31b25fb45f
hanjoondev <hanjoondev@gmail.com>
2022-04-22 03:34:50 +0100

    Initial Commit python solutions 2. 3. 13_2. 5

--- leetcode/L01023.py
+++ leetcode/L01023.py
@@ -1,9 +1,30 @@
 from time import perf_counter_ns as ns
+from re import split as sp
 
 
 class Solution:
     def camelMatch(self, queries: list[str], pattern: str) -> list[bool]:
-        return 0
+        ans = []
+        p = sp('(?=[A-Z])', pattern)[1:]
+        d = [{c: subp.count(c) for c in subp if c.islower()}
+                for subp in p]
+        for q in queries:
+            q = sp('(?=[A-Z])', q)[1:]
+            if len(q) != len(p):
+                ans.append(False)
+            elif any(p[i] not in q[i] for i in range(len(q))):
+                failed = False
+                for i, subq in enumerate(q):
+                    td = {c: subq.count(c) for c in subq if c.islower()}
+                    if any(v > td.get(k, 0) for k, v in d[i].items()):
+                        ans.append(False)
+                        failed = True
+                        break
+                if not failed:
+                    ans.append(True)
+            else:
+                ans.append(True)
+        return ans
 
 
 if __name__ == '__main__':
@@ -12,7 +33,7 @@ if __name__ == '__main__':
     tests = (
         (["FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"], "FB", [True, False, True, True, False]),
         (["FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"], "FoBa", [True, False, True, False, False]),
-        (["FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"], "FoBaT", [False, True, False, False, False]),
+        (["FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"], "FoBaT", [False, True, False, False, False])
     )
     s = Solution()
     for q, p, expected in tests:
