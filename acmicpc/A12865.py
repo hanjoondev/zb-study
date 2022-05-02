a806dbd1e4237a3e8d349e69df7f98b404f125fc
hanjoondev <hanjoondev@gmail.com>
2022-05-02 13:23:19 +0100

    Initial Commit 2. 4. 13_1 3. python solution (dp)

---
+++ acmicpc/A12865.py
@@ -0,0 +1,24 @@
+from sys import stdin as s
+
+
+def solution(n: int, cap: int, weights: list[int], vals: list[int]) -> None:
+    dp = [0] * (cap + 1)
+    for i in range(1, n + 1):
+        for w in range(cap, 0, -1):
+            if weights[i - 1] <= w:
+                dp[w] = max(dp[w], dp[w - weights[i - 1]] + vals[i - 1])
+    print(dp[cap])
+
+
+def reader():
+    n, k = map(int, s.readline().split())
+    weights, values = [], []
+    for _ in range(n):
+        w, v = map(int, s.readline().split())
+        weights.append(w)
+        values.append(v)
+    solution(n, k, weights, values)
+
+
+if __name__ == '__main__':
+    reader()
