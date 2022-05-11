b8bb654ca924e65ff09d45675ef4d9a578153878
hanjoondev <hanjoondev@gmail.com>
2022-05-11 05:30:34 +0100

    Update A10816 (binary search -> hashmap)

--- acmicpc/A10816.py
+++ acmicpc/A10816.py
@@ -1,19 +1,15 @@
-from bisect import bisect_left as bl, bisect_right as br
 from sys import stdin as s
-
-
-def binary_search(arr, target, lo, hi):
-    if arr[0] > target or arr[-1] < target:
-        return '0'
-    return str(br(arr, target, lo, hi) - bl(arr, target, lo, hi))
 
 
 def reader():
     n = int(s.readline())
-    arr = sorted(list(map(int, s.readline().split())))
+    arr = map(int, s.readline().split())
     m = int(s.readline())
-    print(' '.join(binary_search(arr, num, 0, n)
-                   for num in map(int, s.readline().split())) + '\n')
+    targets = map(int, s.readline().split())
+    d = {}
+    for num in arr:
+        d[num] = d.get(num, 0) + 1
+    print(' '.join(str(d.get(num, 0)) for num in targets) + '\n')
 
 
 if __name__ == '__main__':
