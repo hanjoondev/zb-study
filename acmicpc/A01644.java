27901ce4bacc8908cdad524b33b80fa4863107b3
hanjoondev <hanjoondev@gmail.com>
2022-05-16 10:39:21 +0100

    Update A01644 (using ArrayList, sieve starts from true)

--- acmicpc/A01644.java
+++ acmicpc/A01644.java
@@ -3,36 +3,35 @@ package acmicpc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class A01644 {
-    private static int count = 0;
-
-    private static int[] getPrimes(int n) {
-        if (n < 2) return new int[0];
-        boolean[] sieve = new boolean[n / 2 + 1];
-        for (int i = 3 ; i * i <= n; i += 2)
-            if (!sieve[i / 2])
-                for (int j = i * i; j <= n; j += i * 2)
-                    sieve[j / 2] = true;
-        int[] primes = new int[n / 2 + 2];
-        primes[0] = 2;
-        count++;
-        for (int i = 3; i <= n; i += 2)
-            if (!sieve[i / 2])
-                primes[count++] = i;
-        return primes;
+    private static ArrayList<Integer> getPrimes(int n) {
+        if (n < 2) return new ArrayList<Integer>();
+        boolean[] sieve = new boolean[n + 1];
+        Arrays.fill(sieve, true);
+        for (int comp = 2; comp * comp <= n; comp++)
+            if (sieve[comp])
+                for (int i = comp * 2; i <= n; i += comp)
+                    sieve[i] = false;
+        return new ArrayList<Integer>() {{ 
+            add(2);
+            for (int i = 3; i <= n; i += 2) if (sieve[i]) add(i);
+        }};
     }
 
     private static int twoPointer(int n) {
-        int[] p = getPrimes(n);
-        if (count == 0) return 0;
-        int slow = 0, fast = 0, sum = 0, ans = 0;
-        while (fast <= count) {
+        if (n == 1) return 0;
+        ArrayList<Integer> p = getPrimes(n);
+        int slow = 0, fast = 0, sum = p.get(fast), ans = 0, len = p.size();
+        while (slow <= fast) {
             if (sum < n) {
-                sum += p[fast++];
+                if (++fast == len) break;
+                sum += p.get(fast);
             } else {
-                if (sum == n) ans++;
-                sum -= p[slow++];
+                ans += sum == n ? 1 : 0;
+                sum -= p.get(slow++);
             }
         }
         return ans;
