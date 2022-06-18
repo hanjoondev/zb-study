39c5d5a5e85dee3cf28701c781ef89de06c1cebb
Hanjoon Kim <hanjoondev@gmail.com>
2022-06-17 22:54:13 -0700

    Update A02075 (heapq variant)

--- acmicpc/A02075.py
+++ acmicpc/A02075.py
@@ -1,36 +1,18 @@
 from sys import stdin
-
-
-class Heap:
-    def __init__(self):
-        self.h = []
-
-    def b(self, data):
-        self.h.append(data)
-        p = ((i := len(self.h) - 1) - 1) // 2
-        while i and self.h[p] > self.h[i]:
-            self.h[p], self.h[i] = self.h[i], self.h[p]
-            p = ((i := p) - 1) // 2
-
-    def t(self, data):
-        self.h[0], p, l, r = data, 0, 1, 2
-        i = 1 + (r < (hl := len(self.h)) and self.h[l] > self.h[r])
-        while i < hl and self.h[p] > self.h[i]:
-            self.h[p], self.h[i] = self.h[i], self.h[p]
-            p, l, r = i, 2 * i + 1, 2 * i + 2
-            i = l + (r < hl and self.h[l] > self.h[r])
+from heapq import heappush as hpush, heappop as hpop
 
 
 def reader():
     read = stdin.readline
-    n, h = int(read()) - 1, Heap()
+    n, h = int(read()) - 1, []
     for i in map(int, read().split()):
-        h.b(i)
+        hpush(h, i)
     for _ in range(n):
         for i in map(int, read().split()):
-            if i > h.h[0]:
-                h.t(i)
-    print(h.h[0])
+            if i > h[0]:
+                hpop(h)
+                hpush(h, i)
+    print(h[0])
 
 
 if __name__ == '__main__':
