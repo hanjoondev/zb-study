43fe4a361e946b3af7f2c19ae07a35f1db08511d
Hanjoon Kim <hanjoondev@gmail.com>
2022-06-17 22:53:19 -0700

    Update A02075 custom heap (1,744ms)

--- acmicpc/A02075.py
+++ acmicpc/A02075.py
@@ -5,36 +5,32 @@ class Heap:
     def __init__(self):
         self.h = []
 
-    def insert(self, data):
+    def b(self, data):
         self.h.append(data)
         p = ((i := len(self.h) - 1) - 1) // 2
         while i and self.h[p] > self.h[i]:
-            self.h[p], self.h[i], i = self.h[i], self.h[p], p
-            p = (i - 1) // 2
+            self.h[p], self.h[i] = self.h[i], self.h[p]
+            p = ((i := p) - 1) // 2
 
-    def pop(self):
-        self.h[-1], self.h[0], i = self.h[0], self.h[-1], 0
-        smallest, length = self.h.pop(), len(self.h)
-        while (lft := 2 * i + 1) < length:
-            if (rgt := 2 * i + 2) < length and self.h[lft] > self.h[rgt]:
-                lft = rgt
-            if self.h[lft] > self.h[i]:
-                break
-            self.h[lft], self.h[i], i = self.h[i], self.h[lft], lft
-        return smallest
+    def t(self, data):
+        self.h[0], p, l, r = data, 0, 1, 2
+        i = 1 + (r < (hl := len(self.h)) and self.h[l] > self.h[r])
+        while i < hl and self.h[p] > self.h[i]:
+            self.h[p], self.h[i] = self.h[i], self.h[p]
+            p, l, r = i, 2 * i + 1, 2 * i + 2
+            i = l + (r < hl and self.h[l] > self.h[r])
 
 
 def reader():
     read = stdin.readline
     n, h = int(read()) - 1, Heap()
-    for i in list(map(int, read().split())):
-        h.insert(i)
+    for i in map(int, read().split()):
+        h.b(i)
     for _ in range(n):
-        for i in list(map(int, read().split())):
+        for i in map(int, read().split()):
             if i > h.h[0]:
-                h.pop()
-                h.insert(i)
-    print(h.pop())
+                h.t(i)
+    print(h.h[0])
 
 
 if __name__ == '__main__':
