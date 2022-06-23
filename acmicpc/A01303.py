ca5568098ec8145396bd96c00e0cdc0f90e76355
hanjoondev <hanjoondev@gmail.com>
2022-06-24 00:28:41 +0900

    Update A01303 minor

--- acmicpc/A01303.py
+++ acmicpc/A01303.py
@@ -2,31 +2,26 @@ from sys import stdin
 
 
 def solution(m: list[str], h: int, w: int) -> str:
-    def dfs(r, c):
-        if not 0 <= r < h or not 0 <= c < w or v[r][c] or m[r][c] != color:
-            return
-        v[r][c] = True
-        dfs(r + 1, c)
-        dfs(r - 1, c)
-        dfs(r, c + 1)
-        dfs(r, c - 1)
-        dfs.count += 1
+    def d(r, c):
+        if not 0 <= r < h or not 0 <= c < w or m[r][c] != color:
+            return 0
+        m[r][c] = None
+        return 1 + d(r + 1, c) + d(r - 1, c) + d(r, c + 1) + d(r, c - 1)
 
-    v, ans = [[False] * w for _ in range(h)], {k: 0 for k in 'WB'}
+    ans = {k: 0 for k in 'WB'}
     for r in range(h):
         for c in range(w):
-            if v[r][c]:
+            if not m[r][c]:
                 continue
-            color, dfs.count = m[r][c], 0
-            dfs(r, c)
-            ans[color] += dfs.count**2
+            color = m[r][c]
+            ans[color] += d(r, c) ** 2
     return ' '.join(map(str, ans.values()))
 
 
 def reader():
     read = stdin.readline
     w, h = map(int, read().split())
-    print(solution([read().strip() for _ in range(h)], h, w))
+    print(solution([[c for c in read().strip()] for _ in range(h)], h, w))
 
 
 if __name__ == '__main__':
