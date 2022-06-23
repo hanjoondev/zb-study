d78a518bc4a001a5d0f2e22221a6a136ee7cec3f
hanjoondev <hanjoondev@gmail.com>
2022-06-23 21:01:20 +0900

    Update A01303 (DFS)

--- acmicpc/A01303.py
+++ acmicpc/A01303.py
@@ -1,24 +1,25 @@
 from sys import stdin
-from collections import deque as dq
 
 
 def solution(m: list[str], h: int, w: int) -> str:
+    def dfs(r, c):
+        if not 0 <= r < h or not 0 <= c < w or v[r][c] or m[r][c] != color:
+            return
+        v[r][c] = True
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
+        dfs.count += 1
+
     v, ans = [[False] * w for _ in range(h)], {k: 0 for k in 'WB'}
     for r in range(h):
         for c in range(w):
             if v[r][c]:
                 continue
-            q, color, count, v[r][c] = dq(), m[r][c], 1, True
-            q.append((r, c))
-            while q:
-                tr, tc = q.popleft()
-                for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-                    nr, nc = tr + dr, tc + dc
-                    if 0 <= nr < h and 0 <= nc < w and m[nr][nc] == color and not v[nr][nc]:
-                        v[nr][nc] = True
-                        q.append((nr, nc))
-                        count += 1
-            ans[color] += count**2
+            color, dfs.count = m[r][c], 0
+            dfs(r, c)
+            ans[color] += dfs.count**2
     return ' '.join(map(str, ans.values()))
 
 
