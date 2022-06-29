5197f56c134b9ecc682b8c7a34f0937dc691892a
hanjoondev <hanjoondev@gmail.com>
2022-06-29 19:38:49 +0900

    Update A02389 (Gradient Descent) though still getting ValueError on submission

--- acmicpc/A02389.py
+++ acmicpc/A02389.py
@@ -1,90 +1,33 @@
 from sys import stdin
-from math import hypot as h
-from random import randint
 
 
-class Point:
-    def __init__(self, x=0, y=0) -> None:
-        self.x = x
-        self.y = y
-
-
-class Circle:
-    def __init__(self, c=Point(), r=0) -> None:
-        self.c = c
-        self.r = r
-
-
-class MEC:
-    def __init__(self, pts: list[Point], length: int) -> None:
-        self.pts = pts
-        self.length = length
-
-    def is_inside(self, c: Circle, p: Point):
-        return h(c.c.x - p.x, c.c.y - p.y) <= c.r
-
-    def is_valid_circle(self, c, pts):
-        for p in pts:
-            if not self.is_inside(c, p):
-                return False
-        return True
-
-    def get_center(self, bx, by, cx, cy):
-        b, c, d = bx * bx + by * by, cx * cx + cy * cy, bx * cy - by * cx
-        return Point((cy * b - by * c) / (2 * d), (bx * c - cx * b) / (2 * d))
-
-    def get_min_circle(self, pts):
-        assert len(pts) <= 3
-        if not pts:
-            return Circle()
-        elif len(pts) == 1:
-            return Circle(pts[0], 0)
-        elif len(pts) == 2:
-            return self.two(pts[0], pts[1])
-        for i in range(3):
-            for j in range(i + 1, 3):
-                c = self.two(pts[i], pts[j])
-                if self.is_valid_circle(c, pts):
-                    return c
-        return self.three(pts[0], pts[1], pts[2])
-
-    def two(self, a, b):
-        c = Point((a.x + b.x) / 2, (a.y + b.y) / 2)
-        return Circle(c, h(a.x - b.x, a.y - b.y) / 2)
-
-    def three(self, a, b, c):
-        d = self.get_center(b.x - a.x, b.y - a.y, c.x - a.x, c.y - a.y)
-        d.x, d.y = d.x + a.x, d.y + a.y
-        return Circle(d, h(d.x - a.x, d.y - a.y))
-
-    def welzl(self, pts: list[Point], r: list[Point], length: int):
-        if not length or len(r) == 3:
-            return self.get_min_circle(r)
-        idx = randint(0, length - 1)
-        p = pts[idx]
-        pts[idx], pts[length - 1] = pts[length - 1], pts[idx]
-        d = self.welzl(pts, r.copy(), length - 1)
-        if self.is_inside(d, p):
-            return d
-        r.append(p)
-        return self.welzl(pts, r.copy(), length - 1)
-
-    def solve(self):
-        c = self.welzl(self.pts.copy(), [], self.length)
-        a = []
-        for n in (round(c.c.x, 10), round(c.c.y, 10), round(c.r, 10)):
-            a.append(str(n) if n % 1 else str(int(n)))
-        return ' '.join(a)
+def solution(pts: list[tuple[int]], xs: int, ys: int, length: int) -> str:
+    fx, fy, p = pts[0][0], pts[0][1], 0.1
+    d = e = f = i = 0
+    for _ in range(25000):
+        d, f, i = (xs - fx)**2 + (ys - fy)**2, 0, 1
+        while i < length:
+            e = (xs - pts[i][0])**2 + (ys - pts[i][1])**2
+            if d < e:
+                d, f = e, i
+            i += 1
+        xs, ys = xs + (pts[f][0] - xs) * p, ys + (pts[f][1] - ys) * p
+        p *= 0.999
+    x, y = round(xs, 10), round(ys, 10)
+    x = f'{x:.10f}' if float(f'{x:.4f}') % 1 else int(x)
+    y = f'{y:.10f}' if float(f'{y:.4f}') % 1 else int(y)
+    return ' '.join(map(str, (x, y, round(d**0.5, 10))))
 
 
 def reader():
     read = stdin.readline
     n = int(read().strip())
-    pts = []
+    pts, xsum, ysum = [], 0.0, 0.0
     for _ in range(n):
         x, y = map(int, read().split())
-        pts.append(Point(x, y))
-    print(MEC(pts, n).solve())
+        xsum, ysum = xsum + x, ysum + y
+        pts.append((x, y))
+    print(solution(pts, xsum / n, ysum / n, n))
 
 
 if __name__ == '__main__':
