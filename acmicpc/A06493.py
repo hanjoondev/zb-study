9b57c5b71e4a53a2fee9390cc0c39308bfb864f6
hanjoondev <hanjoondev@gmail.com>
2022-07-02 08:56:11 +0900

    Update minor A06493, A14906 and add missing data

--- acmicpc/A06493.py
+++ acmicpc/A06493.py
@@ -4,21 +4,19 @@ from sys import stdin
 
 def slimp(s: str) -> bool:
     if len(s) > 2:
-        if match("(AB.+C)", s):
+        if match(r'(AB.+C)', s):
             return slimp(s[2:-1])
-        return match("(A.+C)", s) and match("^([DE]F+)+G$", s[1:-1])
+        return match(r'(A.+C)', s) and match(r'^([DE]F+)+G$', s[1:-1])
     return s == 'AH'
 
 
 def reader():
     read = stdin.readline
-    ans = []
+    ans, p = [], r'^([DE]F+)+G$'
     for _ in range(int(read().rstrip())):
         s = read().rstrip()
-        c = (r := len(s)) - s[::-1].find('C')
-        b, y, n = c < (r := r + 1), 'YES', 'NO'
-        ans.append(y if match("^([DE]F+)+G$", s[c if b else 2:])
-                   and (slimp(s[:c]) if b else s.startswith('AH')) else n)
+        ans.append('YES' if match(p, s[c if (c := s.rfind('C') + 1) else 2:])
+                   and (slimp(s[:c]) if c else s.startswith('AH')) else 'NO')
     print('\n'.join(ans))
 
 
